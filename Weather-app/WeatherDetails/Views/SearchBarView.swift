3b9e0c6157c1eda46bfb64ac2a30cf9dbda85adf
Daniel Medina Sada <daniel.medina.sada@hotmail.com>
2024-08-15 11:44:14 -0500

    Create and add SearchBarUI

---
+++ Weather-app/WeatherDetails/Views/SearchBarView.swift
@@ -0,0 +1,8 @@
+//
+//  SearchBarView.swift
+//  Weather-app
+//
+//  Created by Daniel Medina Sada on 8/15/24.
+//
+
+import Foundation
--- Weather-app/WeatherDetails/WeatherDetailsViewController.swift
+++ Weather-app/WeatherDetails/WeatherDetailsViewController.swift
@@ -11,9 +11,5 @@ class WeatherDetailsViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
 }
-
